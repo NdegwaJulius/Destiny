f044b3f99bf1328b48c5096325107ee8d1074ff8
NdegwaJulius <juliusndegwa2@gmail.com>
2022-08-20 11:14:58 +0300

    Completed challenge

---
+++ android/app/src/main/kotlin/com/plunlix/destiny/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.plunlix.destiny
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/story.dart
@@ -0,0 +1,12 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Story{
+String storyTitle;
+String choice1;
+String choice2;
+
+
+Story({required this.choice1, required this.choice2, required this.storyTitle});
+}
+
